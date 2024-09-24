39fb6b1a2cc66e0cd9cf1764950b695abe899840
Nguyen Duc Minh <nguyenducminh051595@gmail.com>
2024-09-24 17:24:49 +0700

    Sep-24-2024:add config tanstack query and add basic post list

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,28 @@
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1>Test TanStack Vue Query</h1>
+    <button @click="refetch">Refetch Data</button>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Error: {{ error.message }}</div>
+    <div v-if="data">
+      <pre>{{ data }}</pre>
+    </div>
   </div>
 </template>
 
-<script>
-import { reactive, toRefs } from 'vue'
+<script setup>
+import { useQuery } from "@tanstack/vue-query";
 
-export default {
-  setup () {
-    const state = reactive({
-      count: 0,
-    })
-  
-    return {
-      ...toRefs(state),
-    }
+const fetcher = async () => {
+  const response = await fetch('https://mock-api-kglw.onrender.com/posts');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
   }
-}
+  return response.json();
+};
+
+const { data, isLoading, error, refetch } = useQuery({
+  queryKey: ['posts'],
+  queryFn: fetcher
+});
 </script>
-
-<style lang="scss" scoped>
-
-</style>
