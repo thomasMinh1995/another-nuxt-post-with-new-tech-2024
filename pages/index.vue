1f72a461410649ee8d3a0f5a2dbbfac21bf2204a
Nguyen Duc Minh <nguyenducminh051595@gmail.com>
2024-09-25 21:35:58 +0700

    Sep-25-2024: add pagination

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,11 @@
     <div v-if="isLoading">Loading...</div>
     <div v-if="error">Error: {{ error.message }}</div>
     <div v-if="data">
-      <pre>{{ data }}</pre>
+      <pre>{{ data.posts }}</pre>
+      <p>Total Posts: {{ totalCount }}</p>
+      <p>Current Page: {{ currentPage }} of {{ totalPages }}</p>
+      <button @click="goToPreviousPage" :disabled="currentPage === 1">Previous</button>
+      <button @click="goToNextPage" :disabled="currentPage === totalPages">Next</button>
     </div>
   </div>
 </template>
@@ -13,16 +17,51 @@
 <script setup>
 import { useQuery } from "@tanstack/vue-query";
 
-const fetcher = async () => {
-  const response = await fetch('https://mock-api-kglw.onrender.com/posts');
+const currentPage = ref(1);
+const limit = 10;
+// Initialize refs for total count and pages
+const totalCount = ref(0);
+const totalPages = ref(0);
+
+const fetcher = async (page) => {
+  const response = await fetch(`https://mock-api-kglw.onrender.com/posts?page=${page}&limit=${limit}`);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
-  return response.json();
+  const data = await response.json();
+  return {
+    posts: data,
+    headers: response.headers
+  };
 };
 
 const { data, isLoading, error, refetch } = useQuery({
-  queryKey: ['posts'],
-  queryFn: fetcher
+  queryKey: ['posts', currentPage],
+  queryFn: () => fetcher(currentPage.value),
+  keepPreviousData: true,
 });
+
+// Watch for changes in data to update totalCount and totalPages
+watch(data, (newData) => {
+  if (newData) {
+    totalCount.value = parseInt(newData.headers.get('x-total-count'), 10);
+    totalPages.value = parseInt(newData.headers.get('x-total-pages'), 10);
+  }
+});
+
+// Functions to navigate pages
+const goToPreviousPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+    refetch();
+  }
+};
+
+const goToNextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++;
+    refetch();
+  }
+};
+
 </script>
