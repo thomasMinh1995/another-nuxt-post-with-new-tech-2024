c43ba22ccf1f91e72c5ea61de5ee88e12b4492b1
Nguyen Duc Minh <nguyenducminh051595@gmail.com>
2024-09-25 22:47:02 +0700

    Sep-25-2024: add store post

---
+++ stores/posts.js
@@ -0,0 +1,42 @@
+export const usePostsStore = defineStore("posts", {
+  state: () => ({
+    currentPage: 1,
+    limit: 10,
+    totalCount: 0,
+    error: null,
+    isLoading: false,
+    posts: [],
+  }),
+  getters: {
+    totalPages: (state) => Math.ceil(state.totalCount / state.limit),
+  },
+  actions: {
+    async fetchPosts() {
+      this.isLoading = true;
+      this.error = null;
+      try {
+        const response = await fetch(
+          `https://mock-api-kglw.onrender.com/posts?page=${this.currentPage}&limit=${this.limit}`
+        );
+        if (!response.ok) throw new Error("Network response was not ok");
+        const data = await response.json();
+        this.posts = data;
+        this.setTotalCount(parseInt(response.headers.get("x-total-count"), 10));
+      } catch (error) {
+        this.setError(error.message);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    setCurrentPage(page) {
+      this.currentPage = page;
+      this.fetchPosts(); 
+    },
+    setTotalCount(count) {
+      this.totalCount = count;
+    },
+    setError(error) {
+        this.error = error;
+      },
+  },
+});
